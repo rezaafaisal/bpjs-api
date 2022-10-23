4769f980537a930c86365ae964852aa5e5af2470
rezafaisal98 <rezha.faizal567@gmail.com>
2022-10-23 14:29:21 +0800

    semi final

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>BPJS | Masuk</title>
     <script src="https://unpkg.com/alpinejs@3.10.4/dist/cdn.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
